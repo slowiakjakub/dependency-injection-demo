5174f564d11cb9b7fcd0f5f2ede08cf41029c565
slowiakjakub <dwabas9@gmail.com>
2020-12-16 11:49:53 +0100

    Add Application class

---
+++ DISample/Application.cs
@@ -0,0 +1,25 @@
+﻿using DemoLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DISample
+{
+    public class Application
+    {
+        IBusinessLogic _businessLogic;
+
+
+        public Application(IBusinessLogic businessLogic)
+        {
+            _businessLogic = businessLogic;
+        }
+
+        public void Run()
+        {
+            _businessLogic.ProcessData();
+        }
+    }
+}
